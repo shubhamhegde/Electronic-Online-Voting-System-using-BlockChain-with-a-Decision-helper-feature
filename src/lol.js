e9239032fa1a3c8159fc8d964238ef0ca98805a3
KritikaKapoor13 <41050755+KritikaKapoor13@users.noreply.github.com>
2019-08-22 23:29:22 +0530

    Add files via upload

---
+++ src/lol.js
@@ -0,0 +1,14 @@
+
+        var names=['Kuldeep Rai Sharma','Ayan Mandal','C G Saji Kumar','C U Rasheed','Gour Chandra Majumder','Henry','K Kalimuthu','K Venkat Ram Babu','Minati Biswas','Paritosh Kumar Haldar','Prakash Minj','S Sudershan Rao','Sanjay Meshack','V V Khalid','Vishal Jolly'];
+
+        var party=['INC','AITC','All India Hindustan Congress Party','IND','IND','IND','IND','IND','IND','IND','BSP','IND','AAP','IND','BJP'];
+        
+        var criminal=['0','0','0','0','0','0','0','0','0','0','0','0','0','0',' 1 '];
+        
+        var age=['52','30','48','34','52','50','66','45','51','61','43','58','49','51','46'];
+        
+        var asset=['Rs 13,22,33,012 ~ 13 Crore+','Rs 72,70,440 ~ 72 Lacs+','Rs 1,20,000 ~ 1 Lacs+','Rs 2,02,808 ~ 2 Lacs+','Rs 60,62,000 ~ 60 Lacs+','Rs 56,459 ~ 56 Thou+','Rs 58,50,000 ~ 58 Lacs+','Rs 3,52,000 ~ 3 Lacs+','Rs 31,98,500 ~ 31 Lacs+','Rs 1,99,16,000 ~ 1 Crore+','Rs 12,50,000 ~ 12 Lacs+','Rs 87,60,000 ~ 87 Lacs+','Rs 9,98,87,782 ~ 9 Crore+','Rs 15,12,000 ~ 15 Lacs+','Rs 8,51,40,573 ~ 8 Crore+'];
+        
+        var liab=['Rs 8,04,50,870 ~ 8 Crore+','Rs 15,00,000 ~ 15 Lacs+','Rs 0 ~ ','Rs 17,00,000 ~ 17 Lacs+','Rs 0 ~ ','Rs 0 ~ ','Rs 0 ~ ','Rs 0 ~ ','Rs 4,00,000 ~ 4 Lacs+','Rs 13,11,000 ~ 13 Lacs+','Rs 2,40,000 ~ 2 Lacs+','Rs 80,00,000 ~ 80 Lacs+','Rs 17,91,076 ~ 17 Lacs+','Rs 3,50,000 ~ 3 Lacs+','Rs 46,33,914 ~ 46 Lacs+'];
+        
+        //console.log(names.toString());
